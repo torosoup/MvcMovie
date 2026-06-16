1fd59ee49c82c7dd42de25d5181bca5f6153e5b4
Thor Oscar <thoroak@ifi.uio.no>
2026-06-16 16:05:47 +0200

    Added data model class Movie.cs - Defines the properties of stored data.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie // Defines the properties of stored data in the database.
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
